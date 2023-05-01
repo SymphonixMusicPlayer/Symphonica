5b0896c3e6f5a7ff63299dbeea503ee88d42883f
V E L Δ <36549174+VELD-Dev@users.noreply.github.com>
2023-05-01 04:34:08 +0200

    Add project files.

---
+++ Symphonix Music Player/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Symphonix
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
